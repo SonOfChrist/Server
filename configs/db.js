8d0aad4e14389c90dbdcfe4876400e7dec001e62
SoftwareDev Stan <mochogestanley80@gmail.com>
2026-04-30 17:34:03 +0300

    ServerCreation

---
+++ configs/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+// Load environment variables from .env file and set them in process.env so they can be accessed in the application
+const connectDB = async () => {
+  try {
+    mongoose.connection.on('connected', () => console.log('Database connected successfully'));
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/TestingDB Projects`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+} 
+export default connectDB;  
